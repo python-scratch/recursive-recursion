ed3393099f473099b32ca9ccaaf95b128db2aaa3
JB Tellez <>
2021-05-18 06:12:42 +0000

    adds recursive solution

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 def reverse_list(lst):
-  return lst[::-1]
+    """recursively reverse a list in place
+    Args:
+        lst: list
+    """
+
+    # often recursive solutions use inner "helper" functions
+    def _reverse(left, right):
+
+        # base case, the point at which recursion should stop
+        if left >= right:
+            return
+
+        # multiple assignment, no need for temp variables
+        lst[left], lst[right] = lst[right], lst[left]
+
+        # base case not met above so recursively call
+        # to get a little closer to solution
+        _reverse(left + 1, right - 1)
+
+    # kick off the helper function
+    _reverse(0, len(lst) - 1)
+
+    return lst
